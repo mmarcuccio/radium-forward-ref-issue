85127898172c442e0f3e93f4f7820fb6e637f16f
Matthew Marcuccio <matthew@marcuccio.com>
2018-10-19 19:57:44 -0700

    Update readme and add comments

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import logo from "./logo.svg";
 import Radium from "radium";
 import "./App.css";
 
+// This works
 const TestRadium = Radium(props => <div>Radium works</div>);
 
+// This works
 const TestForwardRefs = React.forwardRef((props, ref) => (
   <div ref={ref}>Forward refs work</div>
 ));
 
+// This does not render
 const TestBoth = Radium(TestForwardRefs);
 
 class App extends Component {
